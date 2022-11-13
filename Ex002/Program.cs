f130bfa839f00328b56a7b5b358f9a78680ffe6c
SvySor <svysor@gmail.com>
2022-11-13 21:20:16 +0300

    Ex002 corrected to speed up code

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -7,12 +7,12 @@ int WeekDayNumber = Convert.ToInt32(Console.ReadLine());
 if (WeekDayNumber < 7 && WeekDayNumber >1)
 {
     if (WeekDayNumber == 1) Console.WriteLine($"Day number {WeekDayNumber} is Monday");
-    if (WeekDayNumber == 2) Console.WriteLine($"Day number {WeekDayNumber} is Tuesday");
-    if (WeekDayNumber == 3) Console.WriteLine($"Day number {WeekDayNumber} is Wensday");
-    if (WeekDayNumber == 4) Console.WriteLine($"Day number {WeekDayNumber} is Thursday");
-    if (WeekDayNumber == 5) Console.WriteLine($"Day number {WeekDayNumber} is Friday");
-    if (WeekDayNumber == 6) Console.WriteLine($"Day number {WeekDayNumber} is Saturday");
-    if (WeekDayNumber == 7) Console.WriteLine($"Day number {WeekDayNumber} is Sunday");
+    else if (WeekDayNumber == 2) Console.WriteLine($"Day number {WeekDayNumber} is Tuesday");
+    else if (WeekDayNumber == 3) Console.WriteLine($"Day number {WeekDayNumber} is Wensday");
+    else if (WeekDayNumber == 4) Console.WriteLine($"Day number {WeekDayNumber} is Thursday");
+    else if (WeekDayNumber == 5) Console.WriteLine($"Day number {WeekDayNumber} is Friday");
+    else if (WeekDayNumber == 6) Console.WriteLine($"Day number {WeekDayNumber} is Saturday");
+    else if (WeekDayNumber == 7) Console.WriteLine($"Day number {WeekDayNumber} is Sunday");
 }
 else Console.WriteLine($"You are wrong. Week Day Number is the number between 1 and 7. Enter correct number");
 Console.WriteLine();
