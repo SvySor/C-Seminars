96a42052177d1d4e069cb5dac9c67614ea2e4961
SvySor <svysor@gmail.com>
2022-11-13 20:35:28 +0300

    Ex001 is modified to check the right answer

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -1,8 +1,16 @@
 ﻿// Enter data
-Console.Write("Enter number: ");
+Console.Write("Enter first number: ");
 int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
 // Calculation
 int res = a * a;
-// Print out
-Console.Write($"Square of the number {a} is ");
-Console.WriteLine(res);
+if (b == res)
+{
+    Console.Write($"You are right. Square of the number {a} is {b}");
+}
+else
+{
+    Console.Write($"You are wrong. {b} is not the square of the number {a}. Correct answer is ");
+    Console.WriteLine(res);
+}
