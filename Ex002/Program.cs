ee24ef933c8f332210c7a3ba0ffc077bc43e0d05
SvySor <svysor@gmail.com>
2022-11-13 21:12:48 +0300

    Ex002 - program to check weekday created

---
+++ Ex002/Program.cs
@@ -0,0 +1,18 @@
+﻿// Enter data
+
+Console.Write("Number of the weekday: ");
+int WeekDayNumber = Convert.ToInt32(Console.ReadLine());
+
+// Calculation
+if (WeekDayNumber < 7 && WeekDayNumber >1)
+{
+    if (WeekDayNumber == 1) Console.WriteLine($"Day number {WeekDayNumber} is Monday");
+    if (WeekDayNumber == 2) Console.WriteLine($"Day number {WeekDayNumber} is Tuesday");
+    if (WeekDayNumber == 3) Console.WriteLine($"Day number {WeekDayNumber} is Wensday");
+    if (WeekDayNumber == 4) Console.WriteLine($"Day number {WeekDayNumber} is Thursday");
+    if (WeekDayNumber == 5) Console.WriteLine($"Day number {WeekDayNumber} is Friday");
+    if (WeekDayNumber == 6) Console.WriteLine($"Day number {WeekDayNumber} is Saturday");
+    if (WeekDayNumber == 7) Console.WriteLine($"Day number {WeekDayNumber} is Sunday");
+}
+else Console.WriteLine($"You are wrong. Week Day Number is the number between 1 and 7. Enter correct number");
+Console.WriteLine();
