62088f6f1fc8754dff2edc41e9d1c6b040c4ce32
SvySor <svysor@gmail.com>
2022-11-19 00:08:58 +0300

    HomeWork006 created and tested

---
+++ HomeWork006/Program.cs
@@ -0,0 +1,10 @@
+﻿// Enter data
+Console.Write("Enter any number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int div = 2;
+
+// Calculation
+int remainder = a%div;
+if (remainder == 0) Console.Write("Your number is an even number");
+else Console.Write("Your number is an odd number");
+Console.WriteLine();
