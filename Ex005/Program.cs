52467ac75929bf9461a49bc86ce34fc749f5f870
SvySor <svysor@gmail.com>
2022-11-18 22:56:05 +0300

    Ex005 function added

--- Ex005/Program.cs
+++ Ex005/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Enter data
+Console.Write("Enter number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+// Calculation
+int result = PrintQuad(a);
+Console.Write($"Square of your number is {result}");
+
+int PrintQuad (int number)
+{
+return number * number;
+}
