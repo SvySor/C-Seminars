dcfeb6d6d8c66c5a31c09a38837fe4c11694541d
SvySor <svysor@gmail.com>
2022-11-19 00:19:57 +0300

    HomeWork008 is added and tested

---
+++ HomeWork008/Program.cs
@@ -0,0 +1,16 @@
+﻿// Enter data
+
+Console.Write("Enter number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+// Calculation
+int remainder = 0;
+int count = 1;
+int div = 2;
+while (count <= N)
+{
+    remainder = count%div;
+    if (remainder == 0) Console.Write($"{count} ");
+    count++;
+ }
+Console.WriteLine();
