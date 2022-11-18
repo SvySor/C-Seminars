88f34db3a9b821f7736744c3f72104d92fb23262
SvySor <svysor@gmail.com>
2022-11-18 23:55:48 +0300

    HomeWork004 created and tested

---
+++ HomeWork004/Program.cs
@@ -0,0 +1,14 @@
+﻿// Enter data
+Console.Write("Enter first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter third number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+// Calculation
+int max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+Console.Write($"Max is {max}");
+Console.WriteLine();
