20530f416ed84e16853309127d8a54da10b4c6ab
SvySor <svysor@gmail.com>
2022-11-18 23:41:50 +0300

    HomeWork002 created and tested

---
+++ HomeWork002/Program.cs
@@ -0,0 +1,22 @@
+﻿// Enter data
+Console.Write("Enter first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+// Calculation
+if (a == b)
+{
+    Console.Write($"Both numbers are equal {a} == {b}");
+}
+else
+{
+    if (a > b)
+    {
+    Console.Write($"First number {a} is bigger than the second number {b}");
+    }
+    else
+    {
+    Console.Write($"First number {a} is less than the second number {b}");
+    }
+}
+Console.WriteLine();
