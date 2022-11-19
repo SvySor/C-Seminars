1920e6a40538cd8842ff0e515884499133430dc5
SvySor <svysor@gmail.com>
2022-11-19 18:23:15 +0300

    Seminar 2 Homework is added

---
+++ Seminar02_Ex001/Program.cs
@@ -0,0 +1,71 @@
+﻿void Zadacha9 ()
+{
+    // Напишите программу, которая выводит случайное число из отрезка [10,99] и показывает наибольшую цифру числа
+    Random rand = new Random();
+    int number = rand.Next(10,100);
+    int div10 = number / 10;
+    int div1 = number % 10;
+    Console.WriteLine($"Random number between 10 and 99 is: {number}");
+    if (div10 > div1) Console.WriteLine($"Maximum digit os this number is {div10}");
+    if (div10 <= div1) Console.WriteLine($"Maximum digit os this number is {div1}");
+}
+
+
+void Zadacha11 ()
+{
+    // Напишите программу, которая выводит случайное трёхзначное число и удаляет число десятков
+    Random rand = new Random();
+    int number = rand.Next(100,1000);
+    int div100 = number / 100;
+    int div10 = (number - div100*100) / 10;
+    int div1 = number % 10;
+    Console.WriteLine($"Random number between 10 and 99 is: {number}");
+    int NewNumber = div100 * 10 + div1; 
+    if (div10 > div1) Console.WriteLine($"New special number is {NewNumber}");
+}
+
+void Zadacha12 ()
+{
+    // Напишите программу, которая проверяет делимость чисел
+    Console.Write("Enter 1st number: ");
+    int N1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter 2d number: ");
+    int N2 = Convert.ToInt32(Console.ReadLine());
+    int div = N1 % N2;
+    if (div == 0) Console.WriteLine($"{N1} is multiples of {N2}");
+    else Console.WriteLine($"{N1} is not multiples of {N2}. The division is {div}");
+ }
+
+void Zadacha14 ()
+{
+    // Напишите программу, которая проверяет делимость числа на 7 и 23
+    Console.Write("Enter number: ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    int div7 = N % 7;
+    int div23 = N % 23;
+    if (div7 == 0 && div23 == 0) Console.WriteLine($"{N} is multiples of 7 and 23");
+    else Console.WriteLine($"{N} is not multiples of 7 and 23");
+ }
+
+ void Zadacha16 ()
+{
+    // Напишите программу, которая проверяет является ли второе квадратом первого
+    Console.Write("Enter number 1: ");
+    int N1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter number 2: ");
+    int N2 = Convert.ToInt32(Console.ReadLine());
+    int quad1 = N1 * N1;
+    int quad2 = N2 * N2;
+    if (quad1 == N2) Console.WriteLine($"{N2} is the square of {N1}");
+    else
+    {
+        if (quad2 == N1) Console.WriteLine($"{N1} is the square of {N2}");
+        else Console.WriteLine($"{N2} isn't the square of {N1} and {N1} isn't the square of {N2}");
+    }
+ }
+
+//Zadacha9();
+//Zadacha11();
+//Zadacha12();
+//Zadacha14();
+Zadacha16();
---
+++ Seminar02_Homework/Program.cs
@@ -0,0 +1,70 @@
+﻿void Zadacha10 ()
+{
+    // Напишите программу, которая выводит случайное трёхзначное число и на выходе показывает цифру десятков
+    Console.Write("Enter a three-digit number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int div100 = number / 100;
+    int div10 = (number - div100*100) / 10;
+    Console.WriteLine($"The second digit of the number is {div10}");
+}
+
+void Zadacha13 ()
+{
+    // Напишите программу, которая выводит третью цифру заданного числа или сообщает о её отсутствии
+    Console.Write("Enter any number: ");
+    int Number = Convert.ToInt32(Console.ReadLine());
+    int NumberSmall = Number;
+    int count = 0;
+    while (NumberSmall > 10)
+    {
+        NumberSmall = NumberSmall / 10;
+        count++;
+//        Console.WriteLine(count);
+    }
+    count = count + 1;
+//    Console.WriteLine(count);
+    if (count < 3) Console.WriteLine($"There are no 3d digit in this number");
+    else
+    {
+        int Divider = Convert.ToInt32(Math.Pow(10,count-3));
+//        Console.WriteLine(Divider);
+        int NumberFirst_3_Dig = Number / Divider;
+//        Console.WriteLine(NumberFirst_3_Dig);
+        int Dig3 = NumberFirst_3_Dig % 10;
+        Console.WriteLine($"The 3d digit of the number is {Dig3}");
+    }
+
+}
+
+void Zadacha15 ()
+{
+    // Напишите программу, которая проверяет день недели и определяет выходной
+    Console.Write("Enter a number between 1 and 7: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number >= 1 && number <= 7)
+    {
+    Console.WriteLine($"The {number} day of the week is {WeekDay(number)}");
+    if (number > 5) Console.WriteLine($"The {WeekDay(number)} is day off");
+    if (number <= 5) Console.WriteLine($"The {WeekDay(number)} is working day");
+    }
+    else Console.WriteLine($"{number} is not a number of the Week day");
+}
+
+string WeekDay(int Day_Of_Week)
+{
+// Метод определения дня недели
+string Day = "Monday";
+if (Day_Of_Week == 1) Day = "Monday";
+else if (Day_Of_Week == 2) Day = "Tuesday";
+else if (Day_Of_Week == 3) Day = "Wenesday";
+else if (Day_Of_Week == 4) Day = "Thursday";
+else if (Day_Of_Week == 5) Day = "Friday";
+else if (Day_Of_Week == 6) Day = "Saturday";
+else if (Day_Of_Week == 7) Day = "Sunday";
+else Day = "Something is wrong";
+return Day;
+}
+
+//Zadacha10();
+//Zadacha13();
+Zadacha15();
