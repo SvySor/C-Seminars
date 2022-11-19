4edfc51eec7147826b18ae4568b4d1531c15e339
SvySor <svysor@gmail.com>
2022-11-19 12:26:36 +0300

    HomeWork008_var02 added, which is faster

---
+++ HomeWork008_var02/Program.cs
@@ -0,0 +1,16 @@
+﻿// Enter data
+
+Console.Write("Enter number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+// Calculation
+int result = 0; //result of div
+int count = 1; //counter
+int div = 2; // divider
+while (count <= N/div)
+{
+    result = count/div;
+    Console.Write($"{count*div} ");
+    count++;
+ }
+Console.WriteLine();
