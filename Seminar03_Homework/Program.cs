72d50267942cc922dbcd9a00560bc4efef86ca65
SvySor <svysor@gmail.com>
2022-11-20 22:00:40 +0300

    Seminar03_Homework and Seminar03_Samples added

---
+++ Seminar03_Homework/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Seminar02 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void Zadacha19()
+            {
+                // Напишите программу, которая принимает на вход 5-ти значное число и проверяет, является ли оно полиэндром
+                Console.Write("Enter 5 digit number: ");
+                int Number = Convert.ToInt32(Console.ReadLine());
+                if (Number < 10000 | Number > 99999) Console.WriteLine($"You enter wrong number");
+                else
+                {
+                    int div10000 = Number / 10000;
+                    int div1000 = (Number - div10000 * 10000) / 1000;
+                    int div100 = (Number - div10000 * 10000 - div1000 * 1000) / 100;
+                    int div10 = (Number - div10000 * 10000 - div1000 * 1000 - div100 * 100) / 10;
+                    int div1 = Number % 10;
+                    /*
+                    Console.WriteLine(div10000);
+                    Console.WriteLine(div1000);
+                    Console.WriteLine(div100);
+                    Console.WriteLine(div10);
+                    Console.WriteLine(div1);
+                    */
+                    if (div10000 == div1 && div1000 == div10) Console.WriteLine("The number is palindrome.");
+                    else Console.WriteLine("The number is not a palindrome.");
+                }
+            }
+
+            void Zadacha21()
+            // Найдите расстояние между двумя точками в пространстве 3D
+            {
+                Console.WriteLine("Enter coordinates of point A: ");
+                Console.Write("X: ");
+                int Xa = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Y: ");
+                int Ya = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Z: ");
+                int Za = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter coordinates of point B: ");
+                Console.Write("X: ");
+                int Xb = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Y: ");
+                int Yb = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Z: ");
+                int Zb = Convert.ToInt32(Console.ReadLine());
+                int LegX = Xa - Xb;
+                int LegY = Ya - Yb;
+                int LegZ = Za - Zb;
+                double Distance3D = Math.Sqrt((Math.Pow(LegX, 2) + Math.Pow(LegY, 2) + Math.Pow(LegZ, 2)));
+                Console.WriteLine("The distance between points A and B in 3D space is: {0:0.###}", Distance3D);
+                Console.WriteLine();
+            }
+
+            void Zadacha23()
+            // Таблица кубов чисел от 1 до N
+            {
+                Console.Write("Enter number N: ");
+                int N = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter the degree: ");
+                int Deg = Convert.ToInt32(Console.ReadLine());
+                int count = 1;
+                Console.WriteLine();
+                while (count <= Math.Abs(N))
+                {
+                    double count_3 = Math.Pow(Math.Abs(N) / N * count, Deg);
+                    Console.WriteLine($"{Math.Abs(N) / N * count} ^ {Deg} = {count_3}");
+                    count++;
+                }
+                Console.WriteLine();               
+            }
+
+                // Zadacha19();
+                // Zadacha21();
+                Zadacha23();
+
+            }
+        }
+    }
--- Seminar03_Samples/Program.cs
+++ Seminar03_Samples/Program.cs
@@ -1,31 +1,90 @@
-﻿void Zadacha17 ()
+﻿using System;
+
+namespace Seminar02 // Note: actual namespace depends on the project name.
 {
-    // Напишите программу, которая принимает на вход координаты точки (X,Y), такие что X, Y <> 0, и возвращает квадрант расположения точки
-    Random rand = new Random();
-    int x = rand.Next(-10,11);
-    int y = rand.Next(-10,11);
-    Console.WriteLine($"A {x}, {y}");
-    if (x == 0 | y == 0) Console.WriteLine($"Your point is on coordinate line");
-    else if (x > 0 && y > 0) Console.WriteLine($"Your point is in quadrant 1");
-    else if (x > 0 && y < 0) Console.WriteLine($"Your point is in quadrant 2");
-    else if (x < 0 && y < 0) Console.WriteLine($"Your point is in quadrant 3");
-    else if (x < 0 && y > 0) Console.WriteLine($"Your point is in quadrant 4");
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void Zadacha17()
+            {
+                // Напишите программу, которая принимает на вход координаты точки (X,Y), такие что X, Y <> 0, и возвращает квадрант расположения точки
+                Random rand = new Random();
+                int x = rand.Next(-10, 11);
+                int y = rand.Next(-10, 11);
+                Console.WriteLine($"A {x}, {y}");
+                if (x == 0 | y == 0) Console.WriteLine($"Your point is on coordinate line");
+                else if (x > 0 && y > 0) Console.WriteLine($"Your point is in quadrant 1");
+                else if (x > 0 && y < 0) Console.WriteLine($"Your point is in quadrant 2");
+                else if (x < 0 && y < 0) Console.WriteLine($"Your point is in quadrant 3");
+                else if (x < 0 && y > 0) Console.WriteLine($"Your point is in quadrant 4");
+            }
+
+            void Zadacha18()
+            {
+                // Напишите программу, которая по заданному номеру четверти показывает диапазон возможных координат точек
+                Console.Write("Enter number of quarter between 1 and 4: ");
+                int QuarterNumber = Convert.ToInt32(Console.ReadLine());
+                int x = 1;
+                int y = 1;
+                if (QuarterNumber < 1 | QuarterNumber > 4) Console.WriteLine($"You enter wrong number");
+                else
+                {
+                    if (QuarterNumber == 4) y = -1;
+                    else if (QuarterNumber == 3)
+                    {
+                        y = -1;
+                        x = -1;
+                    }
+                    else if (QuarterNumber == 2) x = -1;
+                    Console.WriteLine($"Signs of the ponts are: x = {x}, y = {y}");
+                    Random rand = new Random();
+                    int Ax = rand.Next(1, 100);
+                    int Ay = rand.Next(1, 100);
+                    Console.WriteLine($"For example point A({x * Ax}, {y * Ay})");
+                }
+            }
+
+            void Zadacha21()
+            {
+                // Напишите программу, которая принимает на вход координаты 2-х точек и находит расстояние между ними в 2D
+                Console.WriteLine("Enter coordinates of point A: ");
+                Console.Write("X: ");
+                int Xa = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Y: ");
+                int Ya = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter coordinates of point B: ");
+                Console.Write("X: ");
+                int Xb = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Y: ");
+                int Yb = Convert.ToInt32(Console.ReadLine());
+                int LegX = Xa - Xb;
+                int LegY = Ya - Yb;
+                double Hypotenuse = Math.Sqrt((Math.Pow(LegX, 2) + Math.Pow(LegY, 2)));
+                Console.Write("The distance between points A and B is {0:0.###}", Hypotenuse);
+                Console.WriteLine();
+            }
+
+            void Zadacha22()
+            {
+                // Напишите программу, которая принимает на вход число N и выдаёт таблицу квадратов от 1 до N
+                Console.Write("Enter number N: ");
+                int N = Convert.ToInt32(Console.ReadLine());
+                int count = 1;
+                Console.WriteLine();
+                while (count <= Math.Abs(N))
+                {
+                    double count_2 = Math.Pow(count, 2);
+                    Console.WriteLine($"{Math.Abs(N)/N*count} ^ 2 = {count_2}");
+                    count++;
+                }
+                Console.WriteLine();
+                }
+
+            //Zadacha17();
+            //Zadacha18();
+            //Zadacha21();
+            Zadacha22();
+        }
+    }
 }
-
-void Zadacha18 ()
-{
-    // Напишите программу, которая по заданному номеру четверти показывает диапазон возможных координат точек
-    Random rand = new Random();
-    int x = rand.Next(-10,11);
-    int y = rand.Next(-10,11);
-    Console.WriteLine($"A {x}, {y}");
-    if (x == 0 | y == 0) Console.WriteLine($"Your point is on coordinate line");
-    else if (x > 0 && y > 0) Console.WriteLine($"Your point is in quadrant 1");
-    else if (x > 0 && y < 0) Console.WriteLine($"Your point is in quadrant 2");
-    else if (x < 0 && y < 0) Console.WriteLine($"Your point is in quadrant 3");
-    else if (x < 0 && y > 0) Console.WriteLine($"Your point is in quadrant 4");
-}
-
-
-//Zadacha17();
-Zadacha18();
