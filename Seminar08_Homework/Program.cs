18602d7dcfc63139d9b29bd439c8934bb8905895
SvySor <svysor@gmail.com>
2022-12-10 16:51:09 +0300

    Seminar08_Homework solutions added

--- Seminar08_Homework/Program.cs
+++ Seminar08_Homework/Program.cs
@@ -6,93 +6,131 @@ namespace Homework06 // Двумерные массивы
     {
         static void Main(string[] args)
         {
-            void RoundDigits(double[,] Array, int digits)
+            void DescendingLines(Double[,] Array)
             {
                 int rows = Array.GetLength(0);
                 int columns = Array.GetLength(1);
+                double min = Array[0, 0];
+                for (int i = 0; i < rows; i++)
+                {
+                    min = Array[i, 0];
+                    for (int j = 0; j < columns; j++)
+                    {
+                        for (int k = 0; k < columns - j - 1; k++)
+                        {
+                            if (Array[i, k] < Array[i, k + 1])
+                            {
+                                min = Array[i, k];
+                                Array[i, k] = Array[i, k + 1];
+                                Array[i, k + 1] = min;
+                            }
+                        }
+                    }
+                }
+            }
+
+            //Задача 54. Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+            void Zadacha54()
+            {
+                int rows = 4;
+                int columns = 5;
+                int digits = 2;
+                double[,] numbers = new double[rows, columns];
+                FillArray(numbers, 0, 10);
+                RoundDigits(numbers, digits);
+                PrintArray(numbers);
+                DescendingLines(numbers);
+                Console.WriteLine();
+                PrintArray(numbers);
+            }
+
+            //Задача 56. Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+            void Zadacha56()
+            {
+                int rows = 6;
+                int columns = 8;
+                int digits = 2;
+                double[,] numbers = new double[rows, columns];
+                double[] summColumns = new double[columns];
+                FillArray(numbers, 0, 10);
+                RoundDigits(numbers, digits);
+                PrintArray(numbers);
+                Console.WriteLine();
                 for (int i = 0; i < rows; i++)
                 {
                     for (int j = 0; j < columns; j++)
                     {
-                        Array[i, j] = Math.Round(Array[i, j], digits);
+                        summColumns[i] = summColumns[i] + numbers[i, j];
+
                     }
+                    summColumns[i] = Math.Round(summColumns[i], digits);
+                    Console.WriteLine(summColumns[i]);
                 }
-            }
-
-            // Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами, округлёнными до одного знака.
-
-            void Zadacha47()
-            {
-                int rows = 3;
-                int columns = 4;
-                int digits = 1;
-                double[,] numbers = new double[rows, columns];
-
-                FillArray(numbers, 0, 10);
-                PrintArray(numbers);
-                Console.WriteLine("Можно сделать всё в один проход в одном методе (так будет быстрее), но 2-мя методами лучше видно, как происходит округление:");
-                RoundDigits(numbers, digits);
-                PrintArray(numbers);
-            }
-
-            // Задача 50. Напишите программу, которая на вход принимает индексы элемента в двумерном массиве,
-            // и возвращает значение этого элемента или же указание, что такого элемента нет.
-
-            void Zadacha50()
-            {
-                int rows = 5;
-                int columns = 10;
-                int digits = 2;
-                int indexI = 3;
-                int indexJ = 7;
-                double[,] numbers = new double[rows, columns];
-                FillArray(numbers, 0, 10);
-                RoundDigits(numbers, digits);
-                PrintArray(numbers);
-                Console.WriteLine(); if (indexI < rows && indexJ < columns)
+                int numMinRow = 0;
+                double minSumRow = summColumns[numMinRow];
+                for (int i = 0; i < rows; i++)
                 {
-                    Console.WriteLine($"Элемент с индексами [{indexI}, {indexJ}] = {Math.Round(numbers[indexI, indexJ], digits)}");
-                }
-                else
-                {
-                    Console.WriteLine($"Элемент с индексами [{indexI}, {indexJ}] не существует");
-                }
-            }
-
-            //Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-
-            void Zadacha52()
-            {
-                int rows = 3;
-                int columns = 5;
-                int[,] numbers = new int[rows, columns];
-                FillArray(numbers, 0, 9);
-                PrintArray(numbers);
-                double[] average = new double[columns];
-                for (int j = 0; j < columns; j++)
-                {
-                    Console.Write("____\t");
-                }
-                Console.WriteLine();
-                for (int j = 0; j < columns; j++)
-                {
-                    for (int i = 0; i < rows; i++)
+                    if (minSumRow > summColumns[i])
                     {
-                        average[j] = average[j] + numbers[i, j];
+                        numMinRow = i;
+                        minSumRow = summColumns[i];
                     }
-                    average[j] = average[j] / rows;
-                    Console.Write($"{Math.Round(average[j], 2)}\t");
                 }
+                Console.WriteLine($"Минимальная сумма чисел {minSumRow} находится в {numMinRow + 1} ряду");
             }
 
-            Console.WriteLine("Задача 47");
-            Zadacha47();
+            // Задача 58. Заполните спирально массив 4 на 4 числами от 1 до 16.
+            // 01 02 03 04
+            // 12 13 14 05
+            // 11 16 15 06
+            // 10 09 08 07
+
+            void Zadacha58()
+            {
+                int rows = 4;
+                int columns = 4;
+                int[,] numbers = new int[rows, columns];
+                int size = rows * columns;
+                int k = 1;
+                int begI = 0;
+                int begJ = 0;
+                int endI = rows;
+                int endJ = columns;
+                int i = 0, j = 0;
+                while (k <= size)
+                {
+                    numbers[i, j] = k;
+                    if (i == begI && j < endJ - 1)
+                        j++;
+                    else if (j == endJ - 1 && i < endI - 1)
+                        i++;
+                    else if (i == endI - 1 && j > begJ)
+                        j--;
+                    else
+                        i--;
+                    if ((i == begI + 1) && (j == begJ) && (begJ != endJ - 1))
+                    {
+                        begI++;
+                        endI--;
+                        begJ++;
+                        endJ--;
+                    }
+                    k++;
+                }
+                PrintArray(numbers);
+            }
+
             Console.WriteLine();
-            Console.WriteLine("Задача 50");
-            Zadacha50();
+            Console.WriteLine("Задача 54");
+            Zadacha54();
             Console.WriteLine();
-            Console.WriteLine("Задача 52");
-            Zadacha52(); Console.WriteLine();
+            Console.WriteLine("Задача 56");
+            Zadacha56();
+            Console.WriteLine();
+            Console.WriteLine("Задача 58");
+            Zadacha58();
             Console.WriteLine();
         }
 
@@ -149,6 +187,19 @@ namespace Homework06 // Двумерные массивы
                     Console.Write($"{Array[i, j]}\t");
                 }
                 Console.WriteLine();
+            }
+        }
+
+        static void RoundDigits(double[,] Array, int digits)
+        {
+            int rows = Array.GetLength(0);
+            int columns = Array.GetLength(1);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Array[i, j] = Math.Round(Array[i, j], digits);
+                }
             }
         }
 
